37831ddcb66295bc238c2229e6defaff28571f7a
lubex <hackerlubex@protonmail.com>
2023-05-20 13:45:35 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Subnavigation from "@/components/Navbar/DesktopNav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      erick
+    <main
+    //className="flex min-h-screen flex-col items-center justify-between p-24"
+    >
+      <Subnavigation />
     </main>
-  )
+  );
 }
